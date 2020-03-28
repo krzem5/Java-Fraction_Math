1e6f2877ff07ba8316e0563a73a59c4e163e3e7b
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-03-28 13:49:02 +0100

    Push Update

---
+++ com/krzem/fraction_math/Main.java
@@ -0,0 +1,14 @@
+package com.krzem.fraction_math;
+
+
+
+public class Main{
+	public static void main(String[] args){
+		new Main();
+	}
+
+
+	public Main(){
+		System.out.println(new Fraction(8L).log(new Fraction(2L)));
+	}
+}
